ced292f91a429b509f99a5b441aa1fea3cc236c6
Antonina Graur <124353835+AntoninaGraur@users.noreply.github.com>
2023-05-13 21:51:54 +0200

    authoriz js

---
+++ src/js/autoriz_modal.js
@@ -0,0 +1,22 @@
+
+(() => {
+  const autorizMenu = document.querySelector('.autoriz-menu-container');
+  const openMenuBtn = document.querySelector('.autoriz-open-menu');
+  const closeMenuBtn = document.querySelectorAll('.autoriz-close-menu');
+
+  const toggleMenu = () => {
+    const isMenuOpen =
+      openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
+    openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
+    autorizMenu.classList.toggle('autoriz-is-hidden');
+
+    const scrollLockMethod = !isMenuOpen
+      ? 'disableBodyScroll'
+      : 'enableBodyScroll';
+    bodyScrollLock[scrollLockMethod](document.body);
+  };
+
+  openMenuBtn.addEventListener('click', toggleMenu);
+  closeMenuBtn.forEach(closeMenuBtn => closeMenuBtn.addEventListener('click', toggleMenu));  
+})();
+
