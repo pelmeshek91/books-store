87a77a958e11ba7b5b541dae9927bdd3a18dd2ea
Vladislavv99 <vladlegur6@gmail.com>
2023-05-14 15:06:25 +0300

    .

--- src/js/modal.js
+++ src/js/modal.js
@@ -12,20 +12,12 @@ async function selectBook(e) {
     return;
   }
   const id = item.getAttribute('data-id');
+  console.log(id);
 
   const res = await fetchBooks(id);
-  const { _id, description, book_image, author, title, buy_links, list_name } =
-    res;
-  const settings = {
-    _id,
-    description,
-    book_image,
-    author,
-    title,
-    buy_links,
-    list_name,
-  };
-  // console.log(res);
+  const { _id } = res;
+
+  console.log(res._id);
 
   const markup = createMarkupForModal(res);
   divchik.innerHTML = markup;
@@ -70,20 +62,26 @@ function createMarkupForModal({
           <div class="book-modal">
             <h3 class="title-book-modal">${title}</h3>
             <p class="author-book-modal">${author}</p>
-            <p class="text-book-modal">${description || "no description"}</p>
+            <p class="text-book-modal">${description || 'no description'}</p>
             <ul class="logo-list">
               <li class="logo-item">
-                <a href="${buy_links[0].url}" target="_new" rel="noopener noreferer" aria-label="link to Amazon">
+                <a href="${
+                  buy_links[0].url
+                }" target="_new" rel="noopener noreferer" aria-label="link to Amazon">
                   <img src="./images/modal/image1@1x.png" alt="Amazon" width="62" height="19"/>
                 </a>
               </li class="logo-item">
               <li>
-                <a href="${buy_links[1].url}" target="_new rel="noopener noreferer" aria-label="link to Apple Books">
+                <a href="${
+                  buy_links[1].url
+                }" target="_new rel="noopener noreferer" aria-label="link to Apple Books">
                   <img src="./images/modal/image2@1x.png" alt="Apple Books" width="32" height="33"/>
                 </a>
               </li>
               <li class="logo-item">
-                <a href="${buy_links[4].url}" target="_new rel="noopener noreferer" aria-label="link to Bookshop">
+                <a href="${
+                  buy_links[4].url
+                }" target="_new rel="noopener noreferer" aria-label="link to Bookshop">
                   <img src="./images/modal/image3@1x.png" alt="Bookshop" width="38" height="36"/>
                   </a>
               </li>
@@ -91,5 +89,5 @@ function createMarkupForModal({
           </div>  
       </div>
           <button class="btn-chose-book active">Add to shopping list</button>
-    </div>`
+    </div>`;
 }
