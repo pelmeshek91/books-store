d190516d2bba6ab83aeedf180b8a3ca0e1b24fec
IVANKA1901 <125952478+IVANKA1901@users.noreply.github.com>
2023-05-14 16:34:57 +0200

    fix the function

--- src/js/support.js
+++ src/js/support.js
@@ -15,7 +15,7 @@ let isExpanded = false;
 let isContainerExpanded = false;
 let visibleItemsCount = getVisibleItemsCount();
 
-// Показую тільки перші видимі елементи
+// ///////////Показую тільки перші сталі видимі елементи
 supportItems.forEach((item, index) => {
   if (index >= visibleItemsCount) {
     item.style.display = 'none';
@@ -28,17 +28,19 @@ supportList.classList.add('show');
 
 function onScrollBtnClick() {
   visibleItemsCount = getVisibleItemsCount();
+  console.log(visibleItemsCount);
   if (!isExpanded && !isContainerExpanded) {
     supportItems.forEach((item, index) => {
       if (index >= visibleItemsCount) {
-        item.style.display = 'flex'; // Показую всі елементи
+        item.style.display = 'flex'; ////////////// Показую всі елементи
       }
     });
+
     scrollBtn.style.transform = 'rotate(180deg)';
   } else {
     supportItems.forEach((item, index) => {
       if (index >= visibleItemsCount) {
-        item.style.display = 'none'; // Ховаю всі елементи крім перших 5
+        item.style.display = 'none'; ///////////////// Ховаю всі елементи крім перших 4
       }
     });
     supportContainer.style.height = 'fit-content';
@@ -49,10 +51,56 @@ function onScrollBtnClick() {
 }
 
 function getVisibleItemsCount() {
-  const windowWidth = window.innerWidth;
-  if (windowWidth >= 768) {
+  const windowWidth = window.screen.width;
+  console.log(windowWidth);
+  if (windowWidth > 768) {
     return 6;
   } else {
     return 4;
   }
 }
+
+// const supportContainer = document.querySelector('.support-container');
+// const supportList = document.querySelector('.support-list');
+// const supportItems = document.querySelectorAll('.support-item');
+// const scrollBtn = document.querySelector('.scroll-btn');
+
+// scrollBtn.addEventListener('click', onScrollBtnClick);
+
+// let isExpanded = false;
+// let isContainerExpanded = false;
+
+// // Показую тільки перші 5 елементів
+// supportItems.forEach((item, index) => {
+//   if (index > 5) {
+//     item.style.display = 'none';
+//   }
+// });
+
+// supportContainer.style.height = 'fit-content';
+// scrollBtn.style.transform = 'rotate(0deg)';
+// supportList.classList.add('show');
+
+// function onScrollBtnClick() {
+//   if (!isExpanded && !isContainerExpanded) {
+//     supportItems.forEach((item, index) => {
+//       if (index > 5) {
+//         item.style.display = 'block'; // Показую всі елементи
+//       }
+//     });
+//     // supportList.classList.remove('show');
+//     scrollBtn.style.transform = 'rotate(180deg)';
+//   } else {
+//     supportItems.forEach((item, index) => {
+//       if (index >= 5) {
+//         item.style.display = 'none'; // Ховаю всі елементи крім перших 5
+//       }
+//     });
+//     supportContainer.style.height = 'fit-content';
+//     scrollBtn.style.transform = 'rotate(0deg)';
+//   }
+//   isExpanded = !isExpanded;
+//   isContainerExpanded = !isContainerExpanded;
+// }
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////
