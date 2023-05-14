dc4c6a9cca3bf2f40a85e62185544ae4a1a6a9b6
Lyuba Pushkina <lyuba.pushkina@gmail.com>
2023-05-14 17:19:53 +0100

    add style categ

--- src/js/createMarkup.js
+++ src/js/createMarkup.js
@@ -3,7 +3,7 @@ export function createMarkup(book) {
   return `<li class="book-card" data-id="${_id}">
         <img class="book-image" src="${book_image}" alt="${list_name}" />
         <h3 class="book-title">${
-          title.length > 20 ? title.slice(0, 20) + '...' : title
+          title.length > 18 ? title.slice(0, 18) + '...' : title
         }</h3>
         <p class="book-author">${author}</p>
       </li>`;
