03fccfef02dd21eefc0ac65650becc38efc67400
Antonina Graur <124353835+AntoninaGraur@users.noreply.github.com>
2023-05-15 18:29:49 +0200

    Change_modal_btns

--- src/js/autoriz_modal.js
+++ src/js/autoriz_modal.js
@@ -21,3 +21,53 @@ function closeModal() {
 
 openBtn.addEventListener('click', openModal);
 closeBtn.addEventListener('click', closeModal);
+
+
+// ===================================================
+const form = document.querySelector('.autoriz-modal-form');
+const singUpBtn = document.querySelector('.autoriz-sing-up-btn');
+const autorizBtn = document.querySelector('.autoriz-sub-btn');
+const autorizText = document.querySelector('.autoriz-text');
+
+async function checkAuth() {
+  const user = await getUser();
+  if (user) {
+    autorizText.innerHTML = `<button class="autoriz-sub-btn" type="submit">sing in</button>`;
+    autorizBtn.classList.add('hidden');
+    singUpBtn.innerHTML = 'sing in';
+    autorizBtn.addEventListener('click', () => {
+     
+    });
+  } else {
+    autorizText.innerHTML = `
+      <button class="autoriz-sub-btn" type="submit">sing up</button>
+      <button class="autoriz-sub-btn" type="submit">sing in</button>`;
+    singUpBtn.classList.remove('hidden');
+    autorizBtn.innerHTML = 'sing up';
+    autorizBtn.addEventListener('click', () => {
+     
+    });
+  }
+}
+
+async function getUser() {
+  const user = localStorage.getItem('user');
+  return user ? JSON.parse(user) : null;
+}
+
+async function setUser(user) {
+  localStorage.setItem('user', JSON.stringify(user));
+}
+
+form.addEventListener('submit', async (event) => {
+  event.preventDefault();
+  const name = form.querySelector('.autoriz-modal-input').value;
+  const email = form.querySelector('.autoriz-email').value;
+  const password = form.querySelector('.autoriz-modal-input[name="autoriz_password"]').value;
+  const user = { name, email, password };
+  await setUser(user);
+  checkAuth();
+});
+
+checkAuth();
+
