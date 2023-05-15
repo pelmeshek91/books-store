ca6f5423df0c25f644028713de971e89c41362b0
maksmvc <124098393+maksmvc@users.noreply.github.com>
2023-05-15 18:02:10 +0300

    halfstyles

--- src/js/store.js
+++ src/js/store.js
@@ -4,33 +4,38 @@ function renderBuyLinks(buyLinks) {
   const logoList = buyLinks
     .filter(buyLink => allowedLinks.includes(buyLink.name))
     .map(buyLink => {
-      let logoImageSrc = '';
+      let iconId = '';
       if (buyLink.name === 'Amazon') {
-        logoImageSrc =
-          require('../images/modal/modal-img.svg') + '#icon-amazon';
+        iconId = 'icon-amazon';
       } else if (buyLink.name === 'Apple Books') {
-        logoImageSrc = require('../images/modal/modal-img.svg') + '#icon-apple';
+        iconId = 'icon-apple';
       } else if (buyLink.name === 'Bookshop') {
-        logoImageSrc = require('../images/modal/modal-img.svg') + '#icon-apple';
+        iconId = 'icon-book_shop';
       }
       return `
-        <li class="logo-item">
-          <a href="${buyLink.url}" target="_new" rel="noopener noreferer" aria-label="link to ${buyLink.name}">
-            <img src="${logoImageSrc}" alt="${buyLink.name}" width="62" height="19"/>
-          </a>
-        </li>
-      `;
+      <li class="logo-item">
+        <a href="${
+          buyLink.url
+        }" target="_new" rel="noopener noreferer" aria-label="link to ${
+        buyLink.name
+      }">
+          <svg class="svg-shop-link" width="62" height="19">
+            <use href="${require('../images/modal/modal-img.svg')}#${iconId}"></use>
+          </svg>
+        </a>
+      </li>
+    `;
     });
 
   return `
-    <ul class="logo-list">
-      ${logoList.join('')}
-    </ul>
-  `;
+  <ul class="logo-list">
+    ${logoList.join('')}
+  </ul>
+`;
 }
 
 function renderBookList() {
-  const bookIdsJson = localStorage.getItem('bookIds');
+  const bookIdsJson = localStorage.getItem('bookId');
   const bookIds = JSON.parse(bookIdsJson) || [];
   const promises = bookIds.map(bookId =>
     fetch(`https://books-backend.p.goit.global/books/${bookId}`)
@@ -42,15 +47,26 @@ function renderBookList() {
       const booksMarkup = data.map(book => {
         return `
           <div class="book">
-            <h2 class="book__title">${book.title}</h2>
-            <p class="book__author">Author: ${book.author}</p>
-            <p class="book__description">${book.description}</p>
-            <img class="book__image" src="${book.book_image}" alt="${
-          book.title
-        }">
+          <img class="book__image" src="${book.book_image}" alt="${book.title}">
+          <div class="tablet">
+          <div class="information"><h2 class="book__title">${book.title}</h2>
+          <p class="book__author">Author: ${book.author}</p>
+          
+          </div><p class="book__description">${book.description}</p></div>
+          <div class="shoping-box">
+
+            
+            
+            
+            
             ${renderBuyLinks(book.buy_links)}
-            <button class="delete-book" data-id="${book._id}">Delete</button>
+            
           </div>
+                    <button class="delete-book" data-id="${
+                      book._id
+                    }">Delete</button>
+          </div>
+
         `;
       });
 
@@ -74,10 +90,10 @@ bookList.addEventListener('click', function (event) {
 });
 
 function removeBook(bookId) {
-  const bookIdsJson = localStorage.getItem('bookIds');
+  const bookIdsJson = localStorage.getItem('bookId');
   let bookIds = JSON.parse(bookIdsJson) || [];
   bookIds = bookIds.filter(id => id !== bookId);
-  localStorage.setItem('bookIds', JSON.stringify(bookIds));
+  localStorage.setItem('bookId', JSON.stringify(bookIds));
 
   // Remove the book from localStorage based on its bookId
   localStorage.removeItem(bookId);
