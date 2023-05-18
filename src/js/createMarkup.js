998ec915992e45f188459fb55e59b0d8e15243a1
Lyuba Pushkina <lyuba.pushkina@gmail.com>
2023-05-18 16:52:19 +0100

    add active class

--- src/js/createMarkup.js
+++ src/js/createMarkup.js
@@ -1,7 +1,7 @@
 export function createMarkup(book) {
   const { book_image, title, author, list_name, _id } = book;
   return `<li class="book-card" data-id="${_id}">
-        <img class="book-image" src="${book_image}" alt="${list_name}" />
+        <img class="book-image" src="${book_image}" loading="lazy" alt="${list_name}" />
         <h3 class="book-title">${
           title.length > 16 ? title.slice(0, 16) + '...' : title
         }</h3>
--- src/js/markupCategories.js
+++ src/js/markupCategories.js
@@ -2,8 +2,8 @@ export const markupCategories = data => {
   const markup = data.reduce(
     (acc, { list_name }) =>
       acc +
-      `<li class='item-category'><button type="button" class='btn-categoty'>${list_name}</button></li>`,
-    '<li class="item-category"><button type="button" class="btn-categoty active-text">All categories</button></li>'
+      `<li class='item-category'><button type="button" class='btn-category'>${list_name}</button></li>`,
+    '<li class="item-category"><button type="button" class="btn-category active-text">All categories</button></li>'
   );
   return markup;
 };
